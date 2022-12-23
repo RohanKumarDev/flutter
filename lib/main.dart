b3f9581f06b1a1ae79050ca4f2d06499378c348c
RohanKumarDev <rohankumar93118@gmail.com>
2022-12-23 20:45:45 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       title: "Hello Friends",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Rohan Kumar"),
+          title: Text("Rohan Kumarr"),
         ),
         body: Center(
           child: Text(wordPair.asPascalCase),
